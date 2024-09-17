1f04d7ec4e65f11c9e6a77cc815f7c5c33f7a3d9
lucas-sysa <crocettilucas@gmail.com>
2024-09-17 14:05:52 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+        case '.mp4':
+            contentType = 'video/mp4';
+            break;
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                fs.readFile(path.join(__dirname, '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                });
+            } else {
+                res.writeHead(500);
+                res.end('Server Error');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
+});
+
+server.listen(port, () => console.log(`Server running at http://localhost:${port}/`));
